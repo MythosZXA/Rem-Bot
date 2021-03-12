e7b0b991152c8dc9e383fa663a760b6d039a36cf
MythosZXA <ubw.zeroxa@gmail.com>
2021-03-11 18:54:59 -0600

    redo

---
+++ main.js
@@ -0,0 +1,68 @@
+const Discord = require('discord.js');
+const rem = new Discord.Client();
+const private = require('./private.json');
+const fs = require('fs');
+const commands = require('./commands.js');
+const rpgCommands = require('./rpgCommands.js');
+const genshinCommands = require('./genshinCommands');
+const profile = require('./profile.js');
+// const mysql = require('mysql');
+
+const prefix = 'Rem';
+let rpgProfiles = new Map();
+
+// const con = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'Toant6394',
+//   database: 'RPG'
+// });
+
+// con.connect((err) => {
+//   if(err) {
+//     console.log('Error connecting to RPG DB');
+//     return;
+//   }
+//   console.log('Connected to RPG DB');
+// })
+rem.login(private.token);
+rem.on('ready', () => {
+  console.log('Rem is online.');
+  rem.user.setActivity('for \'Rem, help\'', {type: 'WATCHING'});
+  // update rpg profiles
+  fs.readFile('./rpgProfiles.json', (error, data) => {
+    if (error) throw error;
+    let rpgProfilesTable = JSON.parse(data);
+    rpgProfilesTable.table.forEach(hero => {
+      rpgProfiles.set(hero.userID, new profile(hero));
+    });
+  });
+  console.log('RPG profiles updated');
+  // prevent rem from sleeping by pinging
+  setInterval(() => {
+    console.log('Ping');
+  }, 1000*60*60);
+})
+
+
+rem.on('message',(message) => {
+  console.log(message.author.username + ': ' + message.content);
+  if(message.author.bot)
+    return;
+
+  if(message.content.toLowerCase().includes('thanks rem')) {
+    message.channel.send("You're welcome!");
+    return;
+  }
+  if(message.content.includes('😦')) {
+    message.channel.send('😦');
+    return;
+  }
+
+  let arg = message.content.split(/ +/);
+  if(arg[0].toLowerCase() != 'rem,')
+    return;
+  commands[arg[1]]?.(message, rpgProfiles, arg);
+  rpgCommands[arg[1]]?.(message, rpgProfiles, arg);
+  genshinCommands[arg[1]]?.(message);
+});
