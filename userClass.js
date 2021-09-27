b2b703a919c24ca03299d90af4f94e24c9a8c02c
MythosZXA <ubw.zeroxa@gmail.com>
2021-09-27 01:14:34 -0500

    added some bd

--- userClass.js
+++ userClass.js
@@ -2,5 +2,6 @@ module.exports = class user {
     constructor(userString) {
         this.userID = userString.userID;
         this.name = userString.name;
+        this.BD = userString.BD;
     }
 };
