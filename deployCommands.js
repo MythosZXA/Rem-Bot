d66c4280bbe9f2f55cd91be53881434c55b29f8f
MythosZXA <ubw.zeroxa@gmail.com>
2021-11-29 00:56:42 -0600

    updated deployCommands

--- deployCommands.js
+++ deployCommands.js
@@ -1,15 +1,30 @@
 // environment variables
 require('dotenv').config();
-const fs = require('fs');
 const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const fs = require('fs');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./SlashCommands').filter(file => file.endsWith('.js'));
 
-const rest = new REST({ version: '9' }).setToken(process.env.token);
-
 for (const file of commandFiles) {
     const command = require(`./SlashCommands/${file}`);
     commands.push(command.data.toJSON());
-    console.log('Commands saved!');
 }
+
+const rest = new REST({ version: '9' }).setToken(process.env.token);
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.clientId, process.env.guildId),
+            { body: commands },
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
