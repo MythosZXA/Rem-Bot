81d528f152e0daf09d24a5937c694a181301850b
MythosZXA <ubw.zeroxa@gmail.com>
2022-09-18 23:15:06 -0500

    * changed channel from console to rent for client receipt

--- express.js
+++ express.js
@@ -33,7 +33,7 @@ function setupServer(remDB) {
 
 async function processReceipt(formData) {
 	const server = await rem.guilds.fetch('773660297696772096');
-	const consoleChannel = await rem.channels.fetch('911494733828857866');
+	const receiptChannel = await rem.channels.fetch('970920325170753546');
 	const numMembers = formData.numPayers;	
 	const memberNicknames = [];
 	const debtAmts = [];
