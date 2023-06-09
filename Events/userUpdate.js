5ffff85ef53cbccc860ce575a7a34ff89cbfbd2f
MythosZXA <ubw.zeroxa@gmail.com>
2023-06-08 20:48:38 -0500

    * quick fix on userUpdate

--- Events/userUpdate.js
+++ Events/userUpdate.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'userUpdate',
 	many: true,
 	execute(oldUser, newUser, rem) {
-		const user = rem.remDB.get('users').get(oldUser.id);
+		const user = rem.remDB.get('users').get(newUser.id);
 		user.username = newUser.username;
 	}
 };
