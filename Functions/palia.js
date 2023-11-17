d4ebea13bda15b411075b87d7b5d5d755f2a0074
Toan Tran <73968515+MythosZXA@users.noreply.github.com>
2023-11-16 21:46:09 -0600

    42 palia (#44)
    * 42
    * mostly functional palia page (weeklies are good, daily still left)
    * separated CSS files
    
    * 42 palia page
    * daily gift functionality complete
    
    * 42 palia
    * reset daily/weekly functionality
    
    * 42 palia page
    * comments
    
    * #43 palia page
    * Changed reset to 2 hours before midnight
    
    * #43 palia page
    * fixed logic with reset timing'
    
    * #43 palia page
    * passed in missing client parameter
    
    * 42 palia page
    * fixed reset timing
    * temporary workaround for pages to render after login (issue with logout)
    
    * 42 palia page
    * updated bundle from last commit

--- Functions/palia.js
+++ Functions/palia.js
@@ -1,22 +1,28 @@
 const { secsToMidnight } = require('./specialDaysFunctions.js');
 
 function setupResetTimer(rem) {
-	setTimeout(() => {
-		const dayOfWeek = new Date().getDay();
-
-		rem.remDB.get('palia_gifts').forEach(giftInfo => {
-			// Reset daily
-			giftInfo['gifted'] = 0;
-
-			// Reset weekly if sunday
-			if (dayOfWeek === 0) {
-				giftInfo['gift1'] = 0;
-				giftInfo['gift2'] = 0;
-				giftInfo['gift3'] = 0;
-				giftInfo['gift4'] = 0;
-			}
-		});
-	}, secsToMidnight() - (1000 * 60 * 60)); // An hour before midnight CST (9pm PST)
+	// Setup reset timer if the day hasn't reset
+	if (secsToMidnight() >= ((60 * 60 * 2))) {
+		setTimeout(() => {
+			const dayOfWeek = new Date().getDay();
+	
+			rem.remDB.get('palia_gifts').forEach(giftInfo => {
+				// Reset daily
+				giftInfo['gifted'] = 0;
+	
+				// Reset weekly if sunday
+				if (dayOfWeek === 0) {
+					['gift1','gift2','gift3','gift4'].forEach(gift => giftInfo[gift] = 0);
+				}
+			});
+		}, 1000 * (secsToMidnight() - (60 * 60 * 2))); // Two hours before midnight CST (9pm PST)
+	}
+	// Setup reset timer when tomorrow comes
+	else {
+		setTimeout(() => {
+			setupResetTimer(rem);
+		}, 1000 * (secsToMidnight() + 60));
+	}
 }
 
 module.exports = {
